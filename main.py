d8af332c5c1486b23d43592db8a035b3124e2292
Obl1Que <s.dezhin@mail.ru>
2022-11-04 22:03:28 +0300

    Dont't working

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-import os
+import os, subprocess
+import ait as autoit
 from steam_guard import getCode
 from parce_log_pass import getLogPass
 
 # Example
-# os.system(f"\"C:\\Program Files (x86)\\Steam\\steam.exe\" -login lol lol")
+path_to_steam = '"C:/Program Files (x86)/Steam/steam.exe" -login ilias_122 Sashok5544'
+
+# os.startfile(path_to_steam)
+# print('1')
+
+subprocess.Popen([r'C:\Program Files (x86)\Steam\steam.exe', '-login', 'ilias_122', 'Sashok5544'])
+
+# autoit.win_wait("Steam Guard")
+# autoit.win_activate("Steam Guard")
+# autoit.win_wait_active("Steam Guard")
+# autoit.send(getCode('C:\\Users\\sdezh\\PycharmProjects\\Obl1Que_Auto_Farm\\76561198191656163.maFile'))
