700cf7fa4d6992197d8febeffe56cdc04d6db68a
Obl1Que <s.dezhin@mail.ru>
2022-11-05 16:57:08 +0300

    Rewritten GetCode function

--- steam_guard.py
+++ steam_guard.py
@@ -1,7 +1,8 @@
 from steampy.guard import generate_one_time_code
+from json import load
 
-def getCode(secret):
-    shared_secret = secret
+def getCode(path):
+    shared_secret = load(open(path))["shared_secret"]
     authentication_code = generate_one_time_code(shared_secret)
 
     return authentication_code
